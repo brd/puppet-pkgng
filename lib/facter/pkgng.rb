b2039eb3a75e1e19ce32f7c0adb142afc1e191e0
Brad Davis <brd@FreeBSD.org>
2013-12-05 16:46:36 -0700

    - Make the test for WITH_PKGNG more absolute

--- lib/facter/pkgng.rb
+++ lib/facter/pkgng.rb
@@ -15,9 +15,9 @@ Facter.add("pkgng_enabled") do
   confine :kernel => "FreeBSD"
 
   setcode do
-    if %x{/usr/bin/make -f /etc/make.conf -VWITH_PKGNG} =~ /(yes|true)/i
+    if %x{/usr/bin/make /usr/share/mk/bsd.port.mk -V WITH_PKGNG} =~ /(yes|true)/i
       "true"
-    end if File.exist?('/etc/make.conf')
+    end
   end
 
 end
